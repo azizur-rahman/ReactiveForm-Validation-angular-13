14efc1939bc2d33428880bae67bb8a6f196b8024
azizur-rahman <azizur.rahman.ru@gmail.com>
2022-02-09 23:07:26 +0600

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
+
+declare function uapa(): any;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'blog';
+
+export class AppComponent implements OnInit {
+  title = 'Bootstrap with angular';
+
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  ngOnInit() {
+    var ss = uapa();
+    console.log(ss);
+  }
+
+
+  loginForm: FormGroup = this.formBuilder.group({
+    user: new FormControl("", [Validators.required]),
+    password: new FormControl("", [Validators.required, Validators.minLength(8)])
+  });
+
+  userLogin() {
+
+    if (this.loginForm.valid) {
+
+      console.warn(this.loginForm.value);
+
+    }
+
+  }
+
+  get info() {
+    return this.loginForm.controls;
+  }
+
+  get user() {
+    return this.loginForm.get('user');
+  }
+
+  get password() {
+    return this.loginForm.get('password')
+  }
+
 }
